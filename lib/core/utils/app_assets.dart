945efd1042876253d3775fad373048c67f82c3d5
Your Name <you@example.com>
2024-10-07 13:48:03 +0300

    add assets files and path

---
+++ lib/core/utils/app_assets.dart
@@ -0,0 +1,8 @@
+abstract class AppAssets {
+  static const robot = "assets/gpt-robot.png";
+  static const onboarding = "assets/onboarding.png";
+  static const sendButton = "assets/send.png";
+  static const splash = "assets/splashscreen.png";
+  static const volumeButton = "assets/volume-high.png";
+
+}
